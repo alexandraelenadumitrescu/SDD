66ce6d507e23761a300c92f8e587d6e1c89e9f91
alexandraelenadumitrescu <134046146+alexandraelenadumitrescu@users.noreply.github.com>
2025-07-24 12:13:32 +0300

    .

--- maimultestructuri/maimultestructuri/Source.c
+++ maimultestructuri/maimultestructuri/Source.c
@@ -19,6 +19,15 @@ struct Masina {
 	char norma;
 };
 typedef struct Masina Masina;
+
+
+//
+
+//2. * *Scrie o funcție `initializare` care primește valorile pentru fiecare atribut și returnează o structură `Masina`. * *
+//
+//*Dacă parametrul `marca` este NULL, câmpul `marca` va rămâne NULL.
+//* Altfel, alocă dinamic spațiu pentru `marca` și copiază valoarea primită.
+//
 Masina init(int id, int nrLocuri, char* marca, float capacitate, char norma) {
 	Masina m;
 
@@ -37,23 +46,26 @@ Masina init(int id, int nrLocuri, char* marca, float capacitate, char norma) {
 	return m;
 
 }
-int main() {
-	
-	
-
-	return 0;
-}
-//
-
-//2. * *Scrie o funcție `initializare` care primește valorile pentru fiecare atribut și returnează o structură `Masina`. * *
-//
-//*Dacă parametrul `marca` este NULL, câmpul `marca` va rămâne NULL.
-//* Altfel, alocă dinamic spațiu pentru `marca` și copiază valoarea primită.
-//
 //3. * *Scrie o funcție `afisare` care primește o structură `Masina` și afișează toate atributele acesteia.* *
 //
+void afisare(Masina m) {
+	printf("%d\n", m.id);
+	printf("%d\n", m.nrLocuri);
+	if (m.marca != NULL) {
+
+		printf("%s\n", m.marca);
+	}
+	printf("%f\n", m.capacitate);
+	printf("%c\n", m.norma);
+
+}
+
 //4. * *Scrie o funcție `afisareVector` care primește un vector de `Masina` și numărul de elemente, și afișează fiecare mașină apelând funcția `afisare`.**
-//
+void afisareVector(Masina* masini, int nr) {
+	for (int i = 0;i < nr;i++) {
+		afisare(masini[i]);
+	}
+}
 //5. * *Scrie o funcție `copiazaPrimeleMasini` care primește : **
 //
 //*un vector de mașini
@@ -62,10 +74,25 @@ int main() {
 //
 //Funcția va returna un vector nou care conține primele `nrElementeCopiate` mașini copiate prin alocare nouă și inițializare.
 //
+Masina* copiazaPrimeleMasini(Masina* masini, int nr, int nrElementeCopiate) {
+	Masina* masiniNou = (Masina*)malloc(sizeof(Masina) * nrElementeCopiate);
+	for (int i = 0;i < nrElementeCopiate;i++) {
+		masiniNou[i] = masini[i];
+	}
+	return masiniNou;
+}
 //6. * *Scrie o funcție `dezalocare` care primește : **
 //
 //*adresa unui vector de mașini
 //* adresa numărului de elemente
+void dezalocare(Masina* masini, int nr) {
+	for (int i = 0;i < nr;i++) {
+		free(masini[i].marca);
+
+	}
+	free(masini);
+	masini = NULL;
+}
 //
 //Funcția va elibera memoria pentru fiecare `marca` și apoi vectorul propriu - zis, setând pointerul la NULL și numărul de elemente la 0.
 //
@@ -73,6 +100,23 @@ int main() {
 //
 //*primește un vector de mașini, numărul de elemente, un prag de capacitate cilindrică
 //* returnează(prin pointeri) un vector nou cu mașinile care au capacitatea mai mare decât pragul
+Masina* copiazaMasiniCuCCMare(Masina* masini, int nr, int prag) {
+	int count = 0;
+	for (int i = 0;i < nr;i++) {
+		if (masini[i].capacitate > prag) {
+			count++;
+		}
+	}
+	Masina* nou = (Masina*)malloc(count * sizeof(Masina));
+	int j = 0;
+	for (int i = 0;i < nr;i++) {
+		if (masini[i].capacitate > prag) {
+			nou[j] = masini[i];
+			j++;
+		}
+	}
+	return nou;
+}
 //
 //8. * *Scrie o funcție `getPrimaMasinaDupaMarca` care primește : **
 //
@@ -82,6 +126,18 @@ int main() {
 //
 //Funcția returnează prima mașină care are marca egală cu cea primită.Dacă nu există, returnează o mașină cu `nrLocuri = 0`, `marca = NULL`, `capacitateC = 0`, `normaPoluare = '0'`.
 //
+Masina getPrimaMasinaDupaMarca(Masina* masini, int nr, char* mc) {
+	for (int i = 0;i < nr;i++) {
+		if (strcmp(masini[i].marca, mc)) {
+			
+		}
+		else {
+			return masini[i];
+		}
+	}
+	
+		return init(0, 0, NULL, 0, 0);
+}
 //9. * *În funcția `main`: * *
 //
 //*alocă un vector dinamic de 3 mașini și inițializează - le cu valori
@@ -103,3 +159,30 @@ int main() {
 //-- -
 //
 //Vrei să - ți scriu și o variantă * *simplificată sau comentată pas cu pas * *?
+int main() {
+	Masina m = init(10, 5, "marca", 100, '1');
+	afisare(m);
+	Masina m1= init(11, 5, "VW", 150, '1');
+	Masina m2 = init(12, 5, "marca", 200, '1');
+	Masina* masini = (Masina*)malloc(sizeof(Masina) * 3);
+	masini[0] = m;
+	masini[1] = m1;
+	masini[2] = m2;
+	afisareVector(masini, 3);
+	Masina* masiniNou = copiazaPrimeleMasini(masini, 3, 2);
+	afisareVector(masiniNou, 2);
+	Masina* x = copiazaMasiniCuCCMare(masini, 3, 0);
+	printf("_--------------------\n");
+
+	afisareVector(x,3);
+	printf("------------");
+	Masina m5 = getPrimaMasinaDupaMarca(masini, 3, "VW");
+	afisare(m5);
+	
+	dezalocare(masini, 3);
+	
+	
+
+
+	return 0;
+}
